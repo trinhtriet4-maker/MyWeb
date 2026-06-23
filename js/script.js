74044eadef6179f08a06065021d45b1399e18b94
trinhtriet4-maker <trinhtriet4@gmail.com>
2026-06-23 14:15:05 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const siteName = "MyWeb";
+let topic = "Du lịch Nha Trang";
+let imageCount = 3;
+let isReady = true;
+
+console.log("Website:", siteName);
+console.log("Chủ đề:", topic);
+console.log("Số ảnh:", imageCount);
+console.log("Đã sẵn sàng dùng JS?", isReady);
+console.log("Kiểu dữ liệu topic:", typeof topic);
